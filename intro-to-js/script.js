d3fb6f7e5643d9127891ca022382b367680dcfa3
Thitithorn Kreingkraikumjorn <nonathome@hotmail.com>
2022-02-17 13:39:39 +0700

    [intro-to-js] first commit

---
+++ intro-to-js/script.js
@@ -0,0 +1,15 @@
+function askMyName(){
+    const name = prompt("What is your name?");
+    if (name != null){
+        document.getElementById('name_h1').innerHTML = "My name is " + name;
+    }
+}
+
+function helloWorld(){
+    const h1message = document.getElementById('name_h1').innerHTML;
+    if (h1message.length <= 11){
+        alert("Tell me your name first man...")
+    } else {
+        alert(h1message + " เลิกกับพี่ ได้ดีทุกคน");
+    }
+}
